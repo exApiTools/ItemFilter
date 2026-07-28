aacfbd1a440907764f620c32b12c10603d2f6d10
instant.sc <code@insc.cc>
2026-07-28 15:07:53 +0200

    Add more weapon data

--- ItemData.cs
+++ ItemData.cs
@@ -120,6 +120,24 @@ public partial class ItemData
 
     public record AttackSpeedData(decimal Base, decimal Total);
 
+    public record DamageData(DamageRange Physical, DamageRange Fire, DamageRange Cold, DamageRange Lightning, DamageRange Chaos)
+    {
+        public DamageRange Total
+        {
+            get
+            {
+                var min = Physical.Min + Fire.Min + Cold.Min + Lightning.Min + Chaos.Min;
+                var max = Physical.Max + Fire.Max + Cold.Max + Lightning.Max + Chaos.Max;
+                return new DamageRange(min, max);
+            }
+        }
+    }
+
+    public record DamageRange(double Min, double Max)
+    {
+        public double Avg => (Min + Max) / 2;
+    }
+
     private readonly Dictionary<string, bool> _hasTagCache = new();
     private readonly Lazy<double> _estimatedValue;
     private readonly Lazy<double?> _estimatedPublicPrice;
@@ -187,6 +205,9 @@ public partial class ItemData
     public ChartData ChartInfo { get; set; } = new ChartData(false, 0, 0, 0, 0, 0, new ChartRoomData("", "", "", 0), 0, new ChartConnectionData(0, false, false, false, false), "None");
 
     public AttackSpeedData AttackSpeed { get; } = new AttackSpeedData(0, 0);
+
+    public DamageData Damage { get; } = new DamageData(new DamageRange(0, 0), new DamageRange(0, 0), new DamageRange(0, 0), new DamageRange(0, 0), new DamageRange(0, 0));
+    public DamageData DPS { get; } = new DamageData(new DamageRange(0, 0), new DamageRange(0, 0), new DamageRange(0, 0), new DamageRange(0, 0), new DamageRange(0, 0));
 
     public PlayerData PlayerInfo => _lastPlayerData = CurrentPlayerData;
 
@@ -494,6 +515,25 @@ public partial class ItemData
                                               decimal.Round(tempAttackSpeedTotal, 2, MidpointRounding.ToPositiveInfinity));
 
             #endregion Attack Speed Calculation
+
+            Damage = new DamageData(new DamageRange(
+                    (weaponComp.DamageMin + LocalStats.GetValueOrDefault(GameStat.LocalMinimumAddedPhysicalDamage)) *
+                    (100 + LocalStats.GetValueOrDefault(GameStat.LocalPhysicalDamagePct)) / 100.0,
+                    (weaponComp.DamageMax + LocalStats.GetValueOrDefault(GameStat.LocalMaximumAddedPhysicalDamage)) *
+                    (100 + LocalStats.GetValueOrDefault(GameStat.LocalPhysicalDamagePct)) / 100.0),
+                new DamageRange(LocalStats.GetValueOrDefault(GameStat.LocalMinimumAddedFireDamage), LocalStats.GetValueOrDefault(GameStat.LocalMaximumAddedFireDamage)),
+                new DamageRange(LocalStats.GetValueOrDefault(GameStat.LocalMinimumAddedColdDamage), LocalStats.GetValueOrDefault(GameStat.LocalMaximumAddedColdDamage)),
+                new DamageRange(LocalStats.GetValueOrDefault(GameStat.LocalMinimumAddedLightningDamage), LocalStats.GetValueOrDefault(GameStat.LocalMaximumAddedLightningDamage)),
+                new DamageRange(LocalStats.GetValueOrDefault(GameStat.LocalMinimumAddedChaosDamage), LocalStats.GetValueOrDefault(GameStat.LocalMaximumAddedChaosDamage)));
+
+            var attackSpeedTotal = (double)AttackSpeed.Total;
+            DPS = new DamageData(
+                new DamageRange(Damage.Physical.Min * attackSpeedTotal, Damage.Physical.Max * attackSpeedTotal),
+                new DamageRange(Damage.Fire.Min * attackSpeedTotal, Damage.Fire.Max * attackSpeedTotal),
+                new DamageRange(Damage.Cold.Min * attackSpeedTotal, Damage.Cold.Max * attackSpeedTotal),
+                new DamageRange(Damage.Lightning.Min * attackSpeedTotal, Damage.Lightning.Max * attackSpeedTotal),
+                new DamageRange(Damage.Chaos.Min * attackSpeedTotal, Damage.Chaos.Max * attackSpeedTotal)
+            );
         }
 
         if (item.TryGetComponent<AttributeRequirements>(out var attributeReqComp))
